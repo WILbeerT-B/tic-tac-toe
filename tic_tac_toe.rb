023c5374cca4956216409b86d33d895ccb09c85a
WILbeerT-B <wilbertbundalian@gmail.com>
2024-01-12 05:27:55 -0800

    Add Board and Game classes

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -9,5 +9,47 @@ class Player
   end
 end
 
-player1 = Player.new(1, 'x')
-player2 = Player.new(2, 'o')
+class Board
+
+  attr_reader :cell
+
+  def initialize
+    @cell = Array(1..9)
+  end
+
+  def display_board
+    puts ""
+    puts " #{cell[0]} | #{cell[1]} | #{cell[2]}"
+    puts "---+---+---"
+    puts " #{cell[3]} | #{cell[4]} | #{cell[5]}"
+    puts "---+---+---"
+    puts " #{cell[6]} | #{cell[7]} | #{cell[8]}"
+    puts ""
+  end
+
+end
+
+class Game
+  
+  attr_reader :message
+
+  def initialize
+    @player1 = Player.new(1, 'x')
+    @player2 = Player.new(2, 'o')
+    @board = Board.new()
+    @message = "Let's play!"
+    
+  end
+
+
+  def play_round
+    puts @message
+    @board.display_board
+    
+  end
+  
+
+end
+
+game = Game.new()
+game.play_round
