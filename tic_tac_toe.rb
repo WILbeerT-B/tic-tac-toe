1fe703044e2b299f613a36ef0a6543ccb0327ac6
WILbeerT-B <wilbertbundalian@gmail.com>
2024-01-25 04:41:51 -0800

    Refactor code, add comments.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,10 +4,9 @@ class Player
   def initialize(number, marker)
     @number = number
     @marker = marker
-    puts "Player #{@number}: Your marker is #{@marker}  "
+    puts "Player #{@number}: Your marker is #{@marker}."
   end
 end
-
 
 class Board
   attr_reader :cell
@@ -31,9 +30,7 @@ class Board
   end
 end
 
-
 class Game
-
   WIN_COMBOS = [ [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2] ]
   attr_reader :message
 
@@ -46,26 +43,29 @@ class Game
   end
 
   def player_select
-    if @count % 2 == 0
-      @player2
-    else
-      @player1
-    end
+    @count % 2 == 0 ? @player2 : @player1
   end
 
   def player_move(current_player)
-    
     print "Player #{current_player.number}. Choose a cell: "
+    
+    #get input from player
     player_choice = gets.chomp.to_i
+
+    #validate the input
     if is_input_valid?(player_choice)
+
+      #validate the cell
       if is_cell_empty?(player_choice)
+        #increment count to switch player
         @count += 1
         @board.update_board(player_choice, current_player.marker)
+
+        #check for winner or draw
         check_winner()
-        #is_board_full?()
-        if is_draw?()
-          puts "Game is draw!"
-        end
+        is_draw?()
+      
+      #display these message after validating the input and cell
       else
         puts "Cell #{player_choice} is not empty, please choose an empty cell."
       end
@@ -84,20 +84,16 @@ class Game
 
   def check_winner()
     WIN_COMBOS.any? do |win_combo|
-      index_1 = win_combo[0]
-      index_2 = win_combo[1]
-      index_3 = win_combo[2]
-      position_1 = @board.cell[index_1]
-      position_2 = @board.cell[index_2]
-      position_3 = @board.cell[index_3]
-      if position_1 == position_2 && position_2 == position_3 && @board.cell[index_1] != ""
+      position_1 = @board.cell[win_combo[0]]
+      position_2 = @board.cell[win_combo[1]]
+      position_3 = @board.cell[win_combo[2]]
+      if position_1 != " " && position_1 == position_2 && position_2 == position_3
         @count = 10
-        if @board.cell[win_combo[0]]  == "X"
-          puts "Player 1 wins!"
-          
-        elsif @board.cell[win_combo[0]]  == "O"
-          puts "Player 2 wins!"
-        end
+
+        #display the winner of the game
+        puts position_1 == "X" ? "Player 1 wins!" : "Player 2 wins!"
+
+        #return true if there is a winner
         true
       end
     end
@@ -108,9 +104,10 @@ class Game
   end
 
   def is_draw?()
-    is_board_full?() && !check_winner()
+    puts "Game is draw!" if is_board_full?() && !check_winner()
   end
 
+  #starts the game
   def play_round
     puts @message
     @board.display_board
@@ -119,7 +116,6 @@ class Game
       @board.display_board
     end
   end
-
 end
 
 tictactoe = Game.new()
